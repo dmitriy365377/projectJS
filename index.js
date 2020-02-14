f4625052ed696a74ccbadebface4ee79111cca3c
dmitriy365377 <tigr365377@gmail.com>
2020-02-15 01:28:47 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,59 @@
+const invoices = require('./invoices.json');
+
+console.log(statement(invoices))
+
+function statement(invoice) {
+    let totalAmount = 0;
+    let volumeCredits = 0;
+    let result = `Счет для ${invoice.customer}\n`;
+
+    const format = new Intl.NumberFormat("ru-RU", {
+        style: "currency", currency: "RUB",
+        minimumFractionDigits: 2
+    }).format;
+
+    for (let perf of invoice.performance) {
+
+        let thisAmount = total(perf)
+
+        // Добавление бонусов
+        volumeCredits += Math.max(perf.audience - 30, 0);
+
+        // Дополнительный бонус за каждые 10 комедий
+        if ("comedy" === perf.type) {
+            volumeCredits += Math.floor(perf.audience / 5);
+        }
+
+        // Вывод строки счета
+        result += `${perf.playId}: ${format(thisAmount / 100)} (${perf.audience} мест)\n`;
+        // result += ` ${perf.audience} мест \n`;
+
+        totalAmount += thisAmount;
+    }
+
+    result += `Итого с вас ${format(totalAmount / 100)}\n `;
+    result += `Вы заработали ${volumeCredits} бонусов\n `;
+
+    return result;
+}
+
+function total(perf) {
+    switch (perf.type) {
+        case "tragedy":
+            thisAmount = 40000;
+            if (perf.audience > 30) {
+                thisAmount += 1000 * (perf.audience - 30);
+            }
+            break;
+        case "comedy":
+            thisAmount = 30000;
+            if (perf.audience > 20) {
+                thisAmount += 10000 + 500 * (perf.audience - 20);
+            }
+            thisAmount += 300 * perf.audience;
+            break;
+        default:
+            throw new Error(`Неизвестный тип: ${perf.type}`);
+    }
+    return thisAmount
+}
